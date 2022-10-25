aa4c05e5211eac150b2d55f7888d19a0ee190b50
Ryu heekyung <beals1129@gmail.com>
2022-10-25 15:20:43 +0900

    feat : add defult background

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import styled from 'styled-components';
+import Login from './pages/Login';
 import './style/common.css';
 
+const BackGround = styled.div`
+
+background-color: var(--background-color);
+width: 100%;
+height: auto;
+overflow: hidden;
+`
 function App() {
   return (
-    <div>
-    </div>
+    <BackGround>
+    </BackGround>
   );
 }
 
