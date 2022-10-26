08b0461769a2e21fc7c4898bbfcadf2bad25f43f
Ryu heekyung <beals1129@gmail.com>
2022-10-26 14:55:53 +0900

    feat :  Add Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,51 @@
-import styled from 'styled-components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import './style/common.css';
 import { createGlobalStyle } from 'styled-components'
+import * as S from './style/App.style'
 import reset from 'styled-reset'
 import Logout from './pages/Logout';
+import Header from './components/Common/Header';
+import Gnb from './components/Common/Gnb';
 const GlobalStyle = createGlobalStyle`
   ${reset}
 `
-const BackGround = styled.div`
 
-background-color: var(--background-color);
-width: 100%;
-height: auto;
-overflow: hidden;
-`
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <BackGround>
-      </BackGround>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
 
+        <S.Container>
+          <Gnb />
+          <Routes>
+
+
+            <Route path='/login' element={<Login />}>
+              {' '}
+            </Route>
+            <Route path='/signup' element={<SignUp />}>
+              {' '}
+            </Route>
+          </Routes>
+
+        </S.Container>
+
+        <S.BackGround>
+          {/* <Route path='/login' element={<Login />}>
+            {' '}
+          </Route> */}
+          {/* <Logout /> */}
+          {/* <SignUp /> */}
+          {/* <Login /> */}
+        </S.BackGround>
+
+
+      </BrowserRouter>
     </>
 
   );
