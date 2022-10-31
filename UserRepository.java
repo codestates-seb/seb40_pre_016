196154aa3c1188c8bfdf8daf19ef9498cdfd6e9f
Jeen Sung <61839458+JEENSUNG@users.noreply.github.com>
2022-10-31 18:22:13 +0900

    Add files via upload

--- UserRepository.java
+++ UserRepository.java
@@ -1,8 +1,11 @@
 package stackoverflow.pre_project.user.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import stackoverflow.pre_project.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+    Page<User> findAll(Pageable pageable);
 }
