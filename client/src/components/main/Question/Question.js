a9a2231a9ef757d10d1df58e772305696b8862d0
tlsrb100 <tlsrb100@gmail.com>
2022-10-31 13:41:46 +0900

    pr : 질문수정 답변수정 페이지 마크업까지

--- client/src/components/main/Question/Question.js
+++ client/src/components/main/Question/Question.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import img from '../../../assets/img/userAvatar.jpeg';
-import * as S from '../../../style/main/Question.style';
-import TagButton from '../Button/TagButtion';
-
+import React from "react";
+import img from "../../../assets/img/userAvatar.jpeg";
+import * as S from "../../../style/main/Question.style";
+import TagButton from "../Button/TagButtion";
+import { Link } from "react-router-dom";
 
 const Question = ({ question }) => {
   return (
     <S.QuestionContainer>
-
       <S.StatContainer>
         <S.Votes>{`${question.votes} votes`}</S.Votes>
         <S.Answers>{`${question.votes} answers`}</S.Answers>
@@ -15,19 +14,23 @@ const Question = ({ question }) => {
       </S.StatContainer>
 
       <S.ContentContainer>
-        <S.Title>{question.title}</S.Title>
+        <S.Title>
+          <Link to="/question">{question.title}</Link>
+        </S.Title>
+
         <S.Content>{question.content}</S.Content>
         <S.FooterContainer>
-          {question.tags.map((el, idx) => (<TagButton key={idx}>{el}</TagButton>))}
+          {question.tags.map((el, idx) => (
+            <TagButton key={idx}>{el}</TagButton>
+          ))}
           <S.UserInfoContainer>
-            <img src={img} alt='userImg' />
-            <div className='userName'>{question.userName}</div>
-            <div className='askCount'>{question.askCount}</div>
-            <div className='userTime'>{question.userTime}  min ago</div>
+            <img src={img} alt="userImg" />
+            <div className="userName">{question.userName}</div>
+            <div className="askCount">{question.askCount}</div>
+            <div className="userTime">{question.userTime} min ago</div>
           </S.UserInfoContainer>
         </S.FooterContainer>
       </S.ContentContainer>
-
     </S.QuestionContainer>
   );
 };
--- client/src/style/main/Question.style.js
+++ client/src/style/main/Question.style.js
@@ -1,50 +1,43 @@
 import styled from "styled-components";
 
 export const QuestionContainer = styled.div`
-font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-border: 1px solid var(--black-075);
-border-top: 0px;
-padding: 16px;
-flex-basis: 100px;
-display: flex;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  border: 1px solid var(--black-075);
+  border-top: 0px;
+  padding: 16px;
+  flex-basis: 100px;
+  display: flex;
 
-@media screen and (max-width: 640px) {
-  flex-direction: column;
-}
-
+  @media screen and (max-width: 640px) {
+    flex-direction: column;
+  }
 `;
-
 
 export const StatContainer = styled.div`
-margin: 0px 16px 4px 0px;
-gap : 6px;
-font-size: 13px;
-display: flex;
-flex-direction: column;
-align-items: flex-end;
-flex-basis: 108px;
-flex-shrink: 0;
+  margin: 0px 16px 4px 0px;
+  gap: 6px;
+  font-size: 13px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  flex-basis: 108px;
+  flex-shrink: 0;
 
-@media screen and (max-width: 640px) {
-  flex-direction: row;
-  flex-basis: 0;
-}
-
-
+  @media screen and (max-width: 640px) {
+    flex-direction: row;
+    flex-basis: 0;
+  }
 `;
 export const Votes = styled.div`
-color: #0C0D0E;
+  color: #0c0d0e;
 `;
-
 
 export const Answers = styled.div`
-color: #6A737C;
+  color: #6a737c;
 `;
 
-
-export const Views = styled.div`
-
-`;
+export const Views = styled.div``;
 
 export const ContentContainer = styled.div`
   flex-shrink: 1;
@@ -52,51 +45,53 @@ export const ContentContainer = styled.div`
 `;
 
 export const Title = styled.div`
-font-size: 17px;
-color: #0A95FF;
-margin-bottom: 5px;
+  font-size: 17px;
+
+  margin-bottom: 5px;
+  & a {
+    text-decoration: none;
+    color: #0a95ff;
+  }
 `;
 
 export const Content = styled.div`
-font-size: 13px;
-color: #3B4045;
-margin-bottom: 8px;
+  font-size: 13px;
+  color: #3b4045;
+  margin-bottom: 8px;
 `;
 
 export const FooterContainer = styled.div`
-font-size: 12px;
-width: 100%;
+  font-size: 12px;
+  width: 100%;
 
-display: flex;
-justify-content: flex-start;
+  display: flex;
+  justify-content: flex-start;
 
-flex-wrap: wrap;
-
+  flex-wrap: wrap;
 `;
 
-
 export const UserInfoContainer = styled.div`
-margin-left: auto;
-display: flex;
+  margin-left: auto;
+  display: flex;
 
-> img{
-  width: 16px;
-  height: 16px;
-}
+  > img {
+    width: 16px;
+    height: 16px;
+  }
 
-div{
-  margin-left: 5px;
-}
+  div {
+    margin-left: 5px;
+  }
 
-.userName{
-  color: #0074CC;
-}
+  .userName {
+    color: #0074cc;
+  }
 
-.askCount{
-  color: #525960;
-}
+  .askCount {
+    color: #525960;
+  }
 
-.userTime{
-  color: #6A737C;
-}
+  .userTime {
+    color: #6a737c;
+  }
 `;
