9238d00636f3b9ccfa44ad1f1d4834a2bc9e5130
Jeen Sung <heee1219@naver.com>
2022-11-01 16:23:03 +0900

    feat: oauth2 및 시큐리티 기능 구현

--- server/src/main/java/stackoverflow/pre_project/config/WebMvcConfig.java
+++ server/src/main/java/stackoverflow/pre_project/config/WebMvcConfig.java
@@ -1,8 +1,19 @@
 package stackoverflow.pre_project.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return new WebSecurityCustomizer() {
+            @Override
+            public void customize(WebSecurity web1) {
+                web1.ignoring().antMatchers("/users", "/api/**", "/user/**", "/comment/**", "/question/**", "/answer/**");
+            }
+        };
+    }
 }
