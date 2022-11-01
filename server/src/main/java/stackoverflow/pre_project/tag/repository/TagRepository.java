3cdf79ef570bc1e7b157a127948fad3043dea8f5
kkg5 <kkg05102@gmail.com>
2022-11-01 18:48:54 +0900

    refactor: 필요없는 부분 제거

--- server/src/main/java/stackoverflow/pre_project/tag/repository/TagRepository.java
+++ server/src/main/java/stackoverflow/pre_project/tag/repository/TagRepository.java
@@ -1,14 +1,10 @@
 package stackoverflow.pre_project.tag.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import stackoverflow.pre_project.tag.entity.Tag;
 
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Page<Tag> findAll(Pageable pageable);
-
     Optional<Tag> findByName(String name);
 }
