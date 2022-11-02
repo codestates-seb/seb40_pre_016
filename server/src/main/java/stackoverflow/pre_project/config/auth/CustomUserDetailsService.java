47a4543daadd5e4c2c72a8ff0810280b106346fe
Jeen Sung <heee1219@naver.com>
2022-11-02 15:27:44 +0900

    Merge pull request #44 from codestates-seb/question_crud
    refactor: OAuth2.0 및 유저 조회 기능

--- server/src/main/java/stackoverflow/pre_project/config/auth/CustomUserDetailsService.java
+++ server/src/main/java/stackoverflow/pre_project/config/auth/CustomUserDetailsService.java
@@ -1,6 +1,7 @@
 package stackoverflow.pre_project.config.auth;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,15 +9,16 @@ import org.springframework.stereotype.Service;
 import stackoverflow.pre_project.user.entity.User;
 import stackoverflow.pre_project.user.repository.UserRepository;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
-public class CustomUserDeatilsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User userEntity = userRepository.findByUsername(username);
+        User userEntity = userRepository.findByEmail(username);
         if (userEntity == null)
             return null;
         return new CustomUserDetails(userEntity);
--- server/src/main/java/stackoverflow/pre_project/user/repository/UserRepository.java
+++ server/src/main/java/stackoverflow/pre_project/user/repository/UserRepository.java
@@ -3,9 +3,9 @@ package stackoverflow.pre_project.user.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import stackoverflow.pre_project.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    Page<User> findAll(Pageable pageable);
+    User findByEmail(String username);
 }
