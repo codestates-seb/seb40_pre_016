e8dd165ab6d15fd769c6166fb93867d1f96e41b8
Cutaku <107826896+Cutaku@users.noreply.github.com>
2022-11-04 14:56:36 +0900

    fix: 누락된 validation 수정

--- server/src/main/java/stackoverflow/pre_project/answer/controller/AnswerController.java
+++ server/src/main/java/stackoverflow/pre_project/answer/controller/AnswerController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import stackoverflow.pre_project.answer.dto.AnswerDto;
 import stackoverflow.pre_project.answer.entity.Answer;
@@ -17,6 +18,7 @@ import javax.validation.constraints.Positive;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Validated
 public class AnswerController {
 
     private final AnswerService answerService;
